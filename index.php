df901b15eb57ecb219678619d8f8bb0b223132ea
robertoAguilar7119 <robertoaguilar6@gmail.com>
2018-03-01 13:05:53 -0800

    Text fixes

--- index.php
+++ index.php
@@ -1295,29 +1295,29 @@
 				<h4 class="m-text12 t-center">
 					Free Delivery Worldwide
 				</h4>
-
-				<a href="#" class="s-text11 t-center">
-					Click here for more info
-				</a>
+                <span class="s-text11 t-center">
+					Free delivery for oders under 15 pounds
+				</span>
+				
 			</div>
 
 			<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 bo2 respon2">
 				<h4 class="m-text12 t-center">
-					30 Days Return
+					Fast Delevery
 				</h4>
 
 				<span class="s-text11 t-center">
-					Simply return it within 30 days for an exchange.
+					Get purchase in under 3 hours
 				</span>
 			</div>
 
 			<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
 				<h4 class="m-text12 t-center">
-					Store Opening
+					Store Open
 				</h4>
 
 				<span class="s-text11 t-center">
-					Shop open from Monday to Sunday
+					Both locations open 24/7
 				</span>
 			</div>
 		</div>
