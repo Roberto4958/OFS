90284bbeddf53f4d491931556fcb126b7539ee04
Grant <33609420+clegggrant@users.noreply.github.com>
2018-04-05 18:15:39 -0700

    Spaced Trash can a little more
    Also made View cart nav to Cart3

--- cart3.php
+++ cart3.php
@@ -65,7 +65,7 @@ session_start();
 							</td>
                             <td class"column-5">'.$weight.' lb</td>
 				            <td class="column-6">$'.$total_price.'</td>
-                            <td class="column-7 p-l-20" style="padding-top:0; padding-bottom:0;"><i data-itemid = "'.$id.'" class="fa fa-trash delete"></i></td>
+                            <td class="column-7 p-l-30" style="padding-top:0; padding-bottom:0;"><i data-itemid = "'.$id.'" class="fa fa-trash delete"></i></td>
                             
 						</tr>';
     }
