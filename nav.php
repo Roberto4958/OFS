4fedfb09d2f781660f88cec7f6ca54f0b729358d
robertoAguilar7119 <robertoaguilar6@gmail.com>
2018-04-13 17:23:49 -0700

    Added logout button to nav, and text fixes

--- nav.php
+++ nav.php
@@ -248,42 +248,20 @@ $displayCart = "";
 				<ul class="main-menu">
 					<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
 						<span class="topbar-child1">
-							Free shipping for standard order over $100
+							Free shipping for orders less than 15 lb
 						</span>
-					</li>
-
-					<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
-						<div class="topbar-child2-mobile">
-							<span class="topbar-email">
-								fashe@example.com
-							</span>
-
-							<div class="topbar-language rs1-select2">
-								<select class="selection-1" name="time">
-									<option>USD</option>
-									<option>EUR</option>
-								</select>
-							</div>
-						</div>
-					</li>
-
-					<li class="item-topbar-mobile p-l-10">
-						<div class="topbar-social-mobile">
-							<a href="#" class="topbar-social-item fa fa-facebook"></a>
-							<a href="#" class="topbar-social-item fa fa-instagram"></a>
-							<a href="#" class="topbar-social-item fa fa-pinterest-p"></a>
-							<a href="#" class="topbar-social-item fa fa-snapchat-ghost"></a>
-							<a href="#" class="topbar-social-item fa fa-youtube-play"></a>
-						</div>
 					</li>
 
 					<li class="item-menu-mobile">
 						<a href="index.php">Home</a>
-						<i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
 					</li>
-
+                    <hr style="margin:0;">
 					<li class="item-menu-mobile">
 						<a href="product.php">Shop</a>
+					</li>
+                    <hr style="margin:0;">
+                    <li class="item-menu-mobile">
+						<a href="logout.php">Log out</a>
 					</li>
 
 				</ul>
