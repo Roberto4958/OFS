a6c52046efcbb03902c565c0b183d248891eeaa9
robertoAguilar7119 <robertoaguilar6@gmail.com>
2018-04-22 15:21:53 -0700

    Prevent user from checking out if weight is over 15 lb

--- subParts/order.php
+++ subParts/order.php
@@ -4,10 +4,11 @@
     $total_weight = 0; 
     $id = $_SESSION['id'];
     $submitButton = "";
+    $toHeavy = '';
     $conn = new mysqli($hn, $un, $pw, $db); //connects to db
     
     if (!$conn->connect_error){ //checks if connected succesfully 
-        $sql = "select i.Name, sum(c.amount) as amount, i.Weight, i.Price, i.CategoryName from Cart c, Items i where i.Id = c.ItemID and c.userID =$id group by i.Name";
+        $sql = "select i.Name, sum(c.amount) as amount, i.Weight, i.Price, i.CategoryName, i.itemName from Cart c, Items i where i.Id = c.ItemID and c.userID =$id group by i.Name";
         
         $result = $conn->query($sql);
         $rows = $result->num_rows;
@@ -19,16 +20,26 @@
             $obj = $result->fetch_array(MYSQLI_ASSOC);
             $total_weight += $obj['amount'] * $obj['Weight'];
             $total_price += $obj['amount'] * $obj['Price'];
-            $cart_items .= generateDiv($obj['Name'], $obj['amount'], $obj['Weight'], $obj['Price'], $obj['CategoryName']); 
+            $cart_items .= generateDiv($obj['Name'], $obj['amount'], $obj['Weight'], $obj['Price'], $obj['CategoryName'], $obj['itemName']); 
         }
         $result->close();
         $conn->close();
         
         $shippingCost = 2*(floor($total_weight/15)); //round down
         $total_price += $shippingCost;
-        if($total_price != 0){
+        if($total_price != 0 && $total_weight <= 15){
             $submitButton = '<button name="SubmitOrder" type="submit" value="Submit" class="btn btn-primary btn-submit-fix">Continue</button>';
         }
+        if($total_weight > 15){
+            $toHeavy = '<div class="form-group">
+                                <div class="col-xs-12">
+                                    <span style="color:#f00;">Order must be less than 15 lb</span>
+                                </div>
+                            </div>
+                            
+                            <div class="form-group"><hr /></div>';
+        }
+        
     }
     else{
         $cart_items = "<h2>We are experiencing server error</h2>";
@@ -37,7 +48,7 @@
     
 
     //@desc: generates html code to display a new row inside the cart table 
-    function generateDiv($name, $amount, $weight, $price, $category){ //old img = item-10.jpg
+    function generateDiv($name, $amount, $weight, $price, $category, $itemName){ //old img = item-10.jpg
         
         $total_price = $amount * $price;
         
@@ -46,7 +57,7 @@
                                     <img class="img-responsive" src="images/'.$category.'/'.$name.'.jpg" />
                                 </div>
                                 <div class="col-sm-6 col-xs-6">
-                                    <div class="col-xs-12"><h3 style="margin-top:0;">'.$name.'</h3></div>
+                                    <div class="col-xs-12"><h3 style="margin-top:0;">'.$itemName.'</h3></div>
                                     <div class="col-xs-12"><small>Quantity:<span> '.$amount.'</span></small></div>
                                     <div class="col-xs-12"><small>Total Weight:<span> '.($weight*$amount).' lb</span></small></div>                 
                                 </div>
@@ -77,14 +88,9 @@
                             </div>
                             
                             <div class="form-group"><hr /></div>
-                            <div class="form-group">
-                                <div class="col-xs-12">
-                                    <strong>Shipping cost</strong>
-                                    <div class="pull-right"><span>$</span><span><?php echo $shippingCost; ?>.00</span></div>
-                                </div>
-                            </div>
-                            
-                            <div class="form-group"><hr /></div>
+                            <?php 
+                                echo $toHeavy; 
+                            ?>
                             <div class="form-group">
                                 <div class="col-xs-12">
                                     <strong>Order Total</strong>
